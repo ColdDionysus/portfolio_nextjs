7ece212540ef07a6e763089e0c3971566099df47
ColdDionysus <gautampukar02@gmail.com>
2023-08-04 09:15:41 +0545

    updated to nextjs

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Exter nal Java script for date and time included in index page
-function currentDate(){
-document.getElementById("timedate").innerHTML = new Date();
-setTimeout(currentDate);
-}
-currentDate();
-
-  
